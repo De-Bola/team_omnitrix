015ce2b277d260f09e0a016143d686b2f6241ed2
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-20 00:53:00 +0300

    added database connection parameters

--- config.php
+++ config.php
@@ -2,14 +2,15 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 //mysql://ba36cf7b289e0a:c8385c30@us-cdbr-iron-east-02.cleardb.net/heroku_f624638ced613be?reconnect=true
+//mysql://b062478d1a3650:b001748f@us-cdbr-iron-east-02.cleardb.net/heroku_0c1823c70ba370e?reconnect=true
 define('DB_SERVER', 'us-cdbr-iron-east-02.cleardb.net');
-define('DB_USERNAME', 'ba36cf7b289e0a');
-define('DB_PASSWORD', 'c8385c30');
-define('DB_NAME', 'heroku_f624638ced613be');
+define('DB_USERNAME', 'b062478d1a3650');
+define('DB_PASSWORD', 'b001748f');
+define('DB_NAME', 'heroku_0c1823c70ba370e');
 define('DB_DRIVER', 'myspli');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'ba36cf7b289e0a', 'c8385c30', 'heroku_f624638ced613be');
+$link = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b062478d1a3650', 'b001748f', 'heroku_0c1823c70ba370e');
  
 // Check connection
 if($link === false){
