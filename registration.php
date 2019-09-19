1dfb730546f4a70442203aab83b0937023e12892
Alex-Pemu <37657432+Alex-Pemu@users.noreply.github.com>
2019-09-19 21:19:18 +0100

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,63 @@
+<?php
+require_once "config.php";
+ob_start();
+session_start();
+
+$fullname=$_POST['fullname'];
+$email= $_POST['email'];
+$mobile= $_POST['mobileno'];
+$name= $_POST['username'];
+$pass= $_POST['apassword'];
+$pass2= $_POST['cpassword'];
+
+
+
+$con= mysqli_connect('localhost', 'root', '');
+mysqli_select_db( $con,'Omnitrix_Userdata');
+
+
+$user = mysqli_query( $con,("SELECT * FROM user WHERE username = '$name'"));
+$mail = mysqli_query( $con,("SELECT * FROM user WHERE email = '$email'"));
+$tel = mysqli_query( $con,("SELECT * FROM user WHERE mobileno = '$mobile'"));
+
+$num = mysqli_num_rows($user);
+$num2 = mysqli_num_rows($mail);
+$num3= mysqli_num_rows($tel);
+
+
+            
+            
+    if ($pass==$pass2 && $num==0 && $num2==0 && $num3==0)
+    
+    {
+       mysqli_query($con,("INSERT INTO user(fullname, email, mobileno, username, apassword, cpassword) VALUES 
+    ('$fullname', '$email', '$mobile', '$name','$pass','$pass2')"));
+         header('location:landing.php');
+    }
+         elseif ($num==1) {
+
+        $message = "Username Already Taken";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+            } elseif ($pass!= $pass2) 
+        
+        {
+         $message2= "PASSWORDS DO NOT MATCH";
+        echo "<script type='text/javascript'>alert('$message2');</script>";
+        } elseif ($num2==1)
+        {
+            $message3 = "Email Address Already In Use";
+            echo "<script type='text/javascript'>alert('$message3');</script>";
+
+        } elseif ($num3==1){
+            $message4 = "Mobile Number In Use";
+           echo "<script type='text/javascript'>alert('$message4');</script>";
+        }
+
+    
+           elseif($fullname || $email || $mobile || $name || $pass || $pass2) {
+            
+                $message5 = "FILL ALL FEILDS";
+                echo "<script type='text/javascript'>alert('$message5');</script>";
+             }    
+
+?>
