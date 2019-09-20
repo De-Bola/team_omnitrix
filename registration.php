cdf1e03b7b184287e008455b02f338fd85fb281e
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-21 01:01:00 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,8 +1,9 @@
 <?php
-require_once "config.php";
+//require_once "config.php";
 ob_start();
 session_start();
-
+if(isset($_POST['signup'])){
+    
 $name= $_POST['username'];
 $email= $_POST['email'];
 $pass= $_POST['apassword'];
@@ -12,8 +13,12 @@ $pass2= $_POST['cpassword'];
 
 //mysql://b062478d1a3650:b001748f@us-cdbr-iron-east-02.cleardb.net/heroku_0c1823c70ba370e?reconnect=true
 
-$con= mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b062478d1a3650', 'b001748f');
-mysqli_select_db( $con,'Omnitrix_Userdata');
+$con= mysqli_connect('127.0.0.1', 'id10949330_b062478d1a3650', 'b001748f');
+mysqli_select_db( $con,'id10949330_omnitrix_data');
+
+if (!$con) { 
+    die("Connection failed: " . mysqli_connect_error()); 
+} 
 
 $user = mysqli_query( $con,("SELECT * FROM user WHERE username = '$name'"));
 $mail = mysqli_query( $con,("SELECT * FROM user WHERE email = '$email'"));
@@ -28,7 +33,7 @@ $num3= mysqli_num_rows($tel);
     {
        mysqli_query($con,("INSERT INTO user(username, email, apassword, fullname, mobileno, cpassword) VALUES 
     ('$name','$email', '$pass', '$fullname', '$mobile', '$pass2')"));
-         header('location:landing.html');
+         header('location:landing.php');
     }
          elseif ($num==1) {
 
@@ -47,13 +52,12 @@ $num3= mysqli_num_rows($tel);
         } elseif ($num3==1){
             $message4 = "Mobile Number In Use";
            echo "<script type='text/javascript'>alert('$message4');</script>";
-        }
+        } elseif($fullname || $email || $mobile || $name || $pass || $pass2) {
+            $message5 = "FILL ALL FEILDS";
+            echo "<script type='text/javascript'>alert('$message5');</script>";
+        } 
+             
+    mysqli_close($con);
 
-    
-           elseif($fullname || $email || $mobile || $name || $pass || $pass2) {
-            
-                $message5 = "FILL ALL FEILDS";
-                echo "<script type='text/javascript'>alert('$message5');</script>";
-             }    
-
+}
 ?>
