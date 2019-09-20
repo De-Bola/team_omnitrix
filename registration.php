dce84caf92dce480bef5854b1128cb8e718b8fad
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-21 01:08:43 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -13,8 +13,8 @@ $pass2= $_POST['cpassword'];
 
 //mysql://b062478d1a3650:b001748f@us-cdbr-iron-east-02.cleardb.net/heroku_0c1823c70ba370e?reconnect=true
 
-$con= mysqli_connect('127.0.0.1', 'id10949330_b062478d1a3650', 'b001748f');
-mysqli_select_db( $con,'id10949330_omnitrix_data');
+$con= mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b062478d1a3650', 'b001748f');
+mysqli_select_db( $con,'omnitrix_data');
 
 if (!$con) { 
     die("Connection failed: " . mysqli_connect_error()); 
