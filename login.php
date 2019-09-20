75cab1d0d4eae8b33a718c1c41eef6aad6ccadd7
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-21 01:25:03 +0300

    Update login.php

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ $user_pass = mysqli_query( $con,("SELECT * FROM heroku_0c1823c70ba370e.user WHER
 $check = mysqli_num_rows($user_pass);
 
 if($check ==1) {
-    header('Location:landing.php' );
+    header('Location:landing.html' );
 } else {
     //header(' Location:login.php ');
 
