7845d87b2787fa2afa79c304ffd46ed8f592945b
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-21 01:16:25 +0300

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ ob_start();
 session_start();
 
 $con= mysqli_connect('127.0.0.1', 'id10949330_b062478d1a3650', 'b001748f');
-mysqli_select_db( $con,'id10949330_omnitrix_data');
+mysqli_select_db( $con,'heroku_0c1823c70ba370e');
 
 if (!$con) { 
     die("Connection failed: " . mysqli_connect_error()); 
@@ -13,7 +13,7 @@ if(isset($_POST['login'])){
 $name= $_POST['username'];
 $pass= $_POST['password'];
 
-$user_pass = mysqli_query( $con,("SELECT * FROM user WHERE username = '$name' and apassword = '$pass'"));
+$user_pass = mysqli_query( $con,("SELECT * FROM heroku_0c1823c70ba370e.user WHERE username = '$name' and apassword = '$pass'"));
 
 $check = mysqli_num_rows($user_pass);
 
